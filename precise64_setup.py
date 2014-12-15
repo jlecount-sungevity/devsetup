6d17b8bf2b2845419fdee0e5c68f7b0150894856
Michael Davis <mdavis@LM-MDAVIS.local>
2014-12-15 11:34:34 -0800

    subprocess to install?

--- precise64_setup.py
+++ precise64_setup.py
@@ -1,8 +1,8 @@
 # for use with ubuntu vagrant minion
+import subprocess
 
 def _bootstrap_salt():
-    # install salt
-    return True
+    subprocess.call(["sudo", "apt-get", "install", "salt"])
 
 def bootstrap():
     _bootstrap_salt()
