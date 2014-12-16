1e7ee6ae894a6de56bbb716665dc75d73f741b80
Michael Davis <mdavis@LM-MDAVIS.local>
2014-12-15 16:36:46 -0800

    add install_items to salt state

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 #!bootstrap/devsetup/bin/python
 import subprocess
 
-def _bootstrap_homebrew():
-    subprocess.call(["ruby", "-e", "$(curl -fsSL
-     https://raw.github.com/Homebrew/homebrew/go/install)"])
-
 def bootstrap():
-    _bootstrap_homebrew()
 
 def Main():
     bootstrap()
